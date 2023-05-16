e0dc4e33e95c3481c1576ba631e95c479a2232d9
Dmitry Leshchev <113301696+Lapamore@users.noreply.github.com>
2023-05-16 09:31:31 +0100

    Add files via upload

---
+++ Olympiad tasks/Potions Olympiad Challenge.py
@@ -0,0 +1,29 @@
+def main():
+    nums = [str(i) for i in range(1, 11)]
+    for num in nums:
+        file = open("test/input/input" + num + ".txt").readlines()
+        act = []
+        for i in range(len(file)):
+            file[i] = file[i].strip()
+            act = file[i].split()
+            string = ""
+
+            for j in range(len(act)):
+                if act[j].isdigit():
+                    act[j] = file[int(act[j]) - 1]
+
+            word = act.pop(0)
+
+            if word == "MIX":
+                string = "MX" + "".join(act) + "XM"
+            if word == "WATER":
+                string = "WT" + "".join(act) + "TW"
+            if word == "DUST":
+                string = "DT" + "".join(act) + "TD"
+            if word == "FIRE":
+                string = "FR" + "".join(act) + "RF"
+
+            file[i] = string
+        print(open("test/output/output" + num + ".txt").readline() == file[-1])
+if __name__ == "__main__":
+    main()
